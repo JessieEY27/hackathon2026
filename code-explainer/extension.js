506b38a7cea6479d11aa494e436814deafda8c97
Jessie Yuen <jyuen55@my.bcit.ca>
2026-03-14 12:01:01 -0700

    Added folder with files for UI

--- code-explainer/extension.js
+++ code-explainer/extension.js
@@ -1,6 +1,8 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-const vscode = require('vscode');
+const vscode = require("vscode");
+const fs = require("fs");
+const path = require("path");
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -9,28 +11,45 @@ const vscode = require('vscode');
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log(
+    'Congratulations, your extension "code-explainer" is now active!',
+  );
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "code-explainer" is now active!');
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with  registerCommand
+  // The commandId parameter must match the command field in package.json
+  const disposable = vscode.commands.registerCommand(
+    "code-explainer.helloWorld",
+    function () {
+      // The code you place here will be executed every time your command is executed
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('code-explainer.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
+      const panel = vscode.window.createWebviewPanel(
+        "codeExplainer",
+        "Code Explainer",
+        vscode.ViewColumn.One,
+        {
+          enableScripts: true,
+        },
+      );
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from code-explainer!');
-	});
+      const htmlPath = path.join(
+        context.extensionPath,
+        "webview",
+        "index.html",
+      );
 
-	context.subscriptions.push(disposable);
+      panel.webview.html = fs.readFileSync(htmlPath, "utf8");
+    },
+  );
+  context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
 function deactivate() {}
 
 module.exports = {
-	activate,
-	deactivate
-}
+  activate,
+  deactivate,
+};
