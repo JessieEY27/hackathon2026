4a6434bfda4ad1f29c6f96ffbb946a0fbdbed130
Jessie Yuen <jyuen55@my.bcit.ca>
2026-03-14 12:01:01 -0700

    Added folder with files for UI

--- code-explainer/extension.js
+++ code-explainer/extension.js
@@ -1,21 +1,43 @@
-const vscode = require('vscode');
+const vscode = require("vscode");
+const fs = require("fs");
+const path = require("path");
 
 /**
- * @param {vscode.ExtensionContext} context
- */
+ 
+@param {vscode.ExtensionContext} context*/
 function activate(context) {
-	console.log('Congratulations, your extension "code-explainer" is now active!');
+  console.log(
+    'Congratulations, your extension "code-explainer" is now active!',
+  );
 
-	const disposable = vscode.commands.registerCommand('code-explainer.helloWorld', function () {
-		vscode.window.showInformationMessage('Hello World from code-explainer!');
-	});
+  const disposable = vscode.commands.registerCommand(
+    "code-explainer.helloWorld",
+    function () {
+      const panel = vscode.window.createWebviewPanel(
+        "codeExplainer",
+        "Code Explainer",
+        vscode.ViewColumn.One,
+        {
+          enableScripts: true,
+        },
+      );
 
-	context.subscriptions.push(disposable);
+      const htmlPath = path.join(
+        context.extensionPath,
+        "webview",
+        "index.html",
+      );
+
+      panel.webview.html = fs.readFileSync(htmlPath, "utf8");
+    },
+  );
+
+  context.subscriptions.push(disposable);
 }
 
 function deactivate() {}
 
 module.exports = {
-	activate,
-	deactivate
+  activate,
+  deactivate,
 };
